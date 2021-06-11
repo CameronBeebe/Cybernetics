e7cba8e243a4e1eff8e53d9bb12c7c3e066975d6
Cameron Beebe <cameronbeebs@gmail.com>
2021-06-10 18:41:52 -0600

    beginning class corrections and consistency changes

--- cybernetics/regulators.py
+++ cybernetics/regulators.py
@@ -25,24 +25,110 @@ class Ashby(Regulator):
     #from cybernetics.train import train
     
     #def __init__(self,goals,game_size,epochs,ran_range,game):
-    def __init__(self):
+    def __init__(self,goals=[],game_size=(1,1),epochs=5,ran_range=10,game=None,skweez=None,history=dict()):
         Regulator.__init__(self)
-        self.goals = []
-        self.game_size = (1,1)
-        self.epochs = 5
-        self.ran_range = 10
-        self.game = None
-        self.skweez = None
-        self.history = dict()
+        self.goals = goals
+        self.game_size = game_size
+        self.epochs = epochs
+        self.ran_range = ran_range
+        self.game = game
+        self.skweez = skweez
+        self.history = history
+        
+        
+        
+        
+    def create_game(self):
+        '''
+        This function takes a size tuple and creates a game matrix of size=(x,y).
+
+        ran_range is to be passed from frontend use of train.
+        '''
+        game_matrix = np.random.randint(self.ran_range, size=self.game_size)
+        print('Created game matrix of size', self.game_size)
+        rows = [i+1 for i in range(self.game_size[0])]
+        
+        # Set object game attribute
+        self.game = game_matrix
+        
+        return pd.DataFrame(data = game_matrix, columns=[i+1 for i in range(game_matrix.shape[1])], index=rows)
+
+    def environment_play(self,dist):
+        '''
+        This function takes a game and a distribution and returns an environmental "play" according to the distribution over the environment's actions in the game matrix.
+        '''
+        return np.random.choice(self.game.index, size=1, p=dist).item()
+
+    def regulator_action(self,probs):
+        '''
+        This function takes a game and a probability distribution and returns a regulator action to try to "control" the environmental action ("disturbance").
+        '''
+        return np.random.choice(self.game.columns, size=1, p=probs).item()
+
+    def prob_calc(self,regulator_dict):
+        '''
+        This function takes a regulator dictionary (composed of actions and pre-probabilities, i.e. a "Polya urn" filled with "action-balls") and converts the urn into probabilities.
+        '''
+        sum_reg = np.array(sum([regulator_dict[i] for i in regulator_dict]))
+        return np.array([regulator_dict[i]/sum_reg for i in regulator_dict])
+
+    def squeeze(self,regulator_dict, urn_list):
+        '''
+        This update function takes two arguments.
+
+        regulator_dict: a regulator defined as a dictionary of key labels (plays or columns) 
+        and integer values (from a distribution or urn).
+
+        urn_list: a list of integers interpreted as the composition of a Polya urn.
+
+        The function calculates the mean of the urn composition, and compares each value
+        in the regulator_dict with the mean.  
+
+        The resulting regulator_dict is updated by incrementing values smaller than the
+        mean, and decrementing values greater than the mean.
+
+        '''
+        mean = np.mean(urn_list)
+        for i in regulator_dict:
+            if regulator_dict[i] >= mean:
+                regulator_dict[i] -= 1
+                #print('squeeze down:', regulator_dict[i])
+            else:
+                regulator_dict[i] += 1
+                #print('squeeze up:', regulator_dict[i])
+
+    def update(self,regulator_dict,action,out,goals,urn_list,skweez=False):
+        '''
+        This update function takes as arguments a regulator, its action, the output in the game matrix of that action, the regulator's goals, and an "urn" composing the regulator's disposition toward actions.  It compares the state of the world output (entry in game matrix) as a result of the action with the regulator's goals.  
+
+        A success variable is set to 1 if the output is in alignment with the regulator's goals.
+
+        Returns success (0 or 1).
+        '''
+        success = 0
+        if out in goals:
+            #print(action)
+            #print("success: reinforced the regulator's action", action, "from", regulator_dict[action], "to", regulator_dict[action]+len(regulator_dict))
+            regulator_dict[action] += len(regulator_dict)**(1/2)
+            success += 1
+            # REMOVED PRINTS FOR LARGE TESTING
+            #print("success!")
+            #print('now we need to recalculate the probabilities according to the reinforced urn')
+        elif skweez:
+            print('fail: squeezing.')
+            squeeze(regulator_dict, urn_list)
+        return success
+
+
         
         
     def train(self):
-        from cybernetics.cybernetics import create_game
-        from cybernetics.cybernetics import environment_play
-        from cybernetics.cybernetics import regulator_action
-        from cybernetics.cybernetics import prob_calc
-        from cybernetics.cybernetics import squeeze
-        from cybernetics.cybernetics import update
+        #from cybernetics.cybernetics import create_game
+        #from cybernetics.cybernetics import environment_play
+        #from cybernetics.cybernetics import regulator_action
+        #from cybernetics.cybernetics import prob_calc
+        #from cybernetics.cybernetics import squeeze
+        #from cybernetics.cybernetics import update
         
         print('self.game:',self.game)
         
@@ -53,7 +139,9 @@ class Ashby(Regulator):
             self.game_size = self.game.shape
         else:
             print('Creating game of size', self.game_size, 'and range', self.ran_range)
-            self.game = create_game(self.game_size,self.ran_range)
+            #self.game = self.create_game(self.game_size,self.ran_range)
+            self.game = self.create_game()
+            
             
             
         print(self.game)
@@ -71,17 +159,17 @@ class Ashby(Regulator):
         while i <= self.epochs:
 
             # Environment chooses play.
-            play = environment_play(self.game,dist)
+            play = self.environment_play(dist)
 
             # Regulator chooses action.
-            action = regulator_action(self.game,probs)
+            action = self.regulator_action(probs)
 
             # Compute state of the world that is output (index in game matrix)
             out = self.game.loc[play,action]
             #print("out:",out)
 
             # Update regulator.
-            successes += update(regulator,action,out,self.goals,urn,skweez=self.skweez)
+            successes += self.update(regulator,action,out,self.goals,urn,skweez=self.skweez)
             
             # Print only every 5 epochs for brevity.
             # REMOVED FOR LARGE EPOCH TESTING.
@@ -90,7 +178,7 @@ class Ashby(Regulator):
             #    print("successes per epoch:",successes / i)
 
             # Recalculate regulator probabilities.
-            probs = prob_calc(regulator)
+            probs = self.prob_calc(regulator)
             #print("updated probs:",probs)
 
             # Log history dict for plotting.
