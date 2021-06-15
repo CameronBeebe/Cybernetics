fed855c3e7d6405781cd8a9ff4d41c16888327db
Cameron Beebe <cameronbeebs@gmail.com>
2021-06-15 11:46:32 -0600

    Advanced test of mpi fixes

--- mpi_regulator_test.py
+++ mpi_regulator_test.py
@@ -1,14 +1,20 @@
 # Run from terminal in env with commands like "mpirun -n 4 python mpi_regulator_test.py"
 from cybernetics.regulators import Ashby
+from mpi4py import MPI
 
-ash = Ashby(epochs=10000)
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+ash = Ashby(epochs=10000, parallelize=True, mpi=True, comm=comm, rank=rank)
 
 ash.game_size = (10,10)
 
 ash.goals.append(3)
 
-ash.parallelize = True
-ash.mpi = True
+#ash.parallelize = True
+#ash.mpi = True
+
+ash.create_game()
 
 ash.train()
 
